b64b09be8d14f6287746450dc6b277df6b94b6fc
ApolloGeneral <kensuzuki0225@gmail.com>
2022-02-19 19:56:33 +0800

    stage one and two creating

---
+++ src/components/stage_one.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const StageOne=()=>{
+    return(
+        <>
+            <View >
+                <Text>Stage One</Text>
+
+            </View>
+        </>
+    )
+}
+
+export default StageOne;
---
+++ src/components/stage_two.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const StageTwo = () => {
+    return (
+        <>
+            <View >
+                <Text>Stage Two</Text>
+
+            </View>
+        </>
+    )
+}
+
+export default StageTwo;
