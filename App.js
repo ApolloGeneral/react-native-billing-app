24c20025423bd397db28568765c24ecd387e4633
ApolloGeneral <kensuzuki0225@gmail.com>
2022-02-19 20:31:24 +0800

    stage view added

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import { MyContext } from './src/context';
-
+import StageOne from './src/components/stage_one';
+import StageTwo from './src/components/stage_two';
 class App extends Component {
   static contextType= MyContext;
   render(){
     return (
-      <View style={styles.container}>
-        <Text>creating something</Text>
-        
-      </View>
+      <ScrollView>
+        <View style={styles.container}>
+          {
+            this.context.state.stage===1 ? 
+            <StageOne></StageOne>
+            :
+            <StageTwo></StageTwo>
+          }
+        </View>
+      </ScrollView>
     );
   }
 }
@@ -22,6 +29,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop:80,
   },
 });
 
